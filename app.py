ca2f1ea4e52fae266ebaac15c5cec59e5bc949de
BhavyaP45 <127996743+BhavyaP45@users.noreply.github.com>
2023-05-06 12:53:05 -0400

    Add app to git

--- app.py
+++ app.py
@@ -1,1 +1,54 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
+#Import Libraries
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+from datetime import datetime
+#Import the local route for this application
+import os
+
+#Configure App
+app = Flask(__name__, template_folder="templates", static_folder='static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    latitude = db.Column(db.Float, nullable = False)
+    longitude = db.Column(db.Float, nullable = False)
+    title = db.Column(db.String(200), nullable = False)
+    description = db.Column(db.String(400), nullable = False)
+    name = db.Column(db.String(100), nullable = False)
+
+    def __repr__(self):
+        return '<Description %r>' %self.id
+
+'''
+Create Database
+with app.app_context():
+    db.create_all()
+'''
+
+@app.route('/', methods = ["POST", "GET"])
+def index():
+    if request.method == "POST":
+        title = request.form['title']
+        content = request.form['content']
+        name = request.form['name']
+
+        new_post = Location(title = title, description = content, name = name)
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            
+            return redirect("/")
+
+        except:
+            return 'There was a problem'
+        
+    else:
+        locations = Location.query.order_by(Location.date_created).all()
+        return render_template('index.html', locations = locations )
+
+if __name__ == "__main__":
+    app.run(debug=True)
